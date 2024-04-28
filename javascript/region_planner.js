36c57e276469bd6a8fd88d4f1fff82df6681547e
huchenlei <chenlei.hu@mail.utoronto.ca>
2024-04-28 15:34:49 -0400

    Sync width/height change

--- javascript/region_planner.js
+++ javascript/region_planner.js
@@ -1,8 +1,9 @@
 
 (function () {
   const cnetAllAccordions = new Set();
+  const appRoot = gradioApp();
   onUiUpdate(() => {
-    gradioApp().querySelectorAll('#controlnet').forEach(accordion => {
+    appRoot.querySelectorAll('#controlnet').forEach(accordion => {
       if (cnetAllAccordions.has(accordion)) return;
       accordion.querySelectorAll('.cnet-region-planner')
         .forEach(container => new RegionPlanner(container, accordion));
@@ -10,15 +11,45 @@
     });
   });
 
-  class RegionPlanner {
-    constructor(container, extension_root) {
+  class GradioSlider {
+    constructor(container) {
       this.container = container;
-      this.extension_root = extension_root
+      this.number_input = container.querySelector('input[type="number"]');
+      this.slider_input = container.querySelector('input[type="range"]');
+    }
+
+    getValue() {
+      return this.number_input.value;
+    }
+
+    onChange(callback) {
+      this.number_input.addEventListener("change", callback);
+      this.slider_input.addEventListener("change", callback);
+    }
+  }
+
+  class A1111Context {
+    constructor(generationType) {
+      this.width_slider = new GradioSlider(appRoot.querySelector(`#${generationType}_width`));
+      this.height_slider = new GradioSlider(appRoot.querySelector(`#${generationType}_height`));
+    }
+  };
+
+  class RegionPlanner {
+    constructor(container, extensionRoot, size) {
+      this.container = container;
+      this.extensionRoot = extensionRoot;
+      this.size = size || 512;
+
+      this.isImg2Img = extensionRoot.querySelector('.cnet-unit-enabled').id.includes('img2img');
+      this.context = new A1111Context(this.isImg2Img ? "img2img" : "txt2img");
       this.stage = new Konva.Stage({
         container: this.container,
-        width: 512,
-        height: 512,
+        width: this.getCanvasWidth(),
+        height: this.getCanvasHeight(),
       });
+      this.context.height_slider.onChange(this.updateCanvasSize.bind(this));
+      this.context.width_slider.onChange(this.updateCanvasSize.bind(this));
 
       const layer = new Konva.Layer();
       this.stage.add(layer);
@@ -37,6 +68,33 @@
         layer.add(unit);
         units.push(unit);
       }
+    }
+
+    getGenerationWidth() {
+      return this.context.width_slider.getValue();
+    }
+
+    getGenerationHeight() {
+      return this.context.height_slider.getValue();
+    }
+
+    getAspectRatio() {
+      return this.getGenerationWidth() / this.getGenerationHeight();
+    }
+
+    getCanvasWidth() {
+      const aspectRatio = this.getAspectRatio();
+      return Math.round(aspectRatio <= 1.0 ? this.size : this.size * aspectRatio);
+    }
+
+    getCanvasHeight() {
+      const aspectRatio = this.getAspectRatio();
+      return Math.round(aspectRatio >= 1.0 ? this.size : this.size / aspectRatio);
+    }
+
+    updateCanvasSize() {
+      this.stage.width(this.getCanvasWidth());
+      this.stage.height(this.getCanvasHeight());
     }
   }
 })();
